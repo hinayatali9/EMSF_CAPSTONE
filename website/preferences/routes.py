12c9e5e17af070686d912547d438007d704e9aea
Guilherme Pita <guilherme.pita@gmail.com>
2023-11-01 19:01:56 -0400

    store data across pages

--- website/preferences/routes.py
+++ website/preferences/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, jsonify, render_template, request, session, url_for
 import pandas as pd
 
 preferences_blueprint = Blueprint("preferences", __name__)
@@ -15,18 +15,15 @@ def render_preferences_page(team_abrv: str):
     return render_template(
         "preferences.html",
         players=df_players["PLAYER_NAME"],
-        num_picks=int(num_picks.iloc[0]),
+        num_picks=int(num_picks.loc[0]),
         team_abrv=team_abrv,
     )
 
 
-@preferences_blueprint.route("/submit_preferences", methods=["POST"])
-def submit_preferences():
+@preferences_blueprint.route("/<team_abrv>/submit_preferences", methods=["POST"])
+def submit_preferences(team_abrv: str):
     input_info = request.get_json()
-    df_min_max_constraints = pd.DataFrame(input_info["min_max_constraints"])
-    new_order = input_info["new_order"]
-    positional_weight = int(input_info["positional_weight"])/5
-    print(
-        df_min_max_constraints, new_order, positional_weight
-    )  # Do something with the new order
-    return "", 204
+    session['min_max_constraints'] = pd.DataFrame(input_info["min_max_constraints"]).to_json()
+    session['new_order'] = input_info["new_order"]
+    session['positional_weight'] = int(input_info["positional_weight"])/5
+    return jsonify({'url': url_for('simulator.draft_simulator', team_abrv=team_abrv)})
